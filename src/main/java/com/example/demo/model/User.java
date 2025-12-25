eff12cfd1e655db0f0a2bec95f0fc5ed5ab2877b
bala-amypo <bala@amypo.in>
2025-12-25 16:54:37 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -16,7 +16,6 @@ public class User {
     @NotBlank(message = "Email is required")
     @Email(message = "Email must be valid")
     private String email;
-    s33
     private String password;
     private String role;
 
